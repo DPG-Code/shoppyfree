78138001747648b3b8194e822897866bc36d37d9
Daniel Prieto <prietogaviriadaniel@gmail.com>
2023-02-20 13:13:21 +0100

    feat: adding cheackout stripe

---
+++ pages/api/checkout.js
@@ -0,0 +1,46 @@
+import connectMongoose from "@/lib/mongoose"
+import Product from "@/models/Product"
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+export default async function handler(req, res){
+  await connectMongoose()
+  
+  if (req.method === 'POST') {
+    try {
+      const {adress, city, name, email} = req.body
+      const productsIds = req.body.products.split(',')
+      const uniqIds = [...new Set(productsIds)]
+      const products = await Product.find({_id: {$in: uniqIds}}).exec()
+
+      let line_items = []
+      for (let productId of uniqIds) {
+        const quantity = productsIds.filter(id => id === productId).length
+        const product = products.find(p => p._id.toString() === productId)
+        line_items.push({
+          price_data: {
+            currency: 'USD',
+            product_data: {
+              name: product.name
+            },
+            unit_amount: Number(product.price) * 100
+          },
+          quantity
+        })
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: line_items,
+        mode: 'payment',
+        customer_email: email,
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      })
+      res.redirect(303, session.url)
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message)
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
--- pages/cart.js
+++ pages/cart.js
@@ -6,6 +6,11 @@ export default function Cart() {
   const { selectedProducts, setSelectedProducts } = useContext(ProductsContext)
   const [cartProducts, setCartProducts] = useState([])
 
+  const [adress, setAdress] = useState('')
+  const [city, setCity] = useState('')
+  const [name, setName] = useState('')
+  const [email, setemail] = useState('')
+
   useEffect(() => {
     const uniqIds = [...new Set(selectedProducts)]
     fetch(`/api/products?ids=${uniqIds.join(',')}`)
@@ -29,7 +34,7 @@ export default function Cart() {
   let delivery = 4.5
   if (selectedProducts.length > 0) {
     for (let id of selectedProducts) {
-      const price = cartProducts.find((p) => p._id === id)?.price
+      const price = cartProducts.find((p) => p._id === id)?.price || 0
       subtotal += Number(price)
     }
   }
@@ -37,27 +42,38 @@ export default function Cart() {
 
   return (
     <Layout>
-      {!cartProducts.length && <p>No products in your shopping cart</p>}
+      {!selectedProducts.length && <p>No products in your shopping cart</p>}
       {cartProducts.length &&
-        cartProducts.map((productInfo) => (
-          <div key={productInfo._id}>
-            <h2>{productInfo.name}</h2>{' '}
-            <div>
-              <button onClick={() => removeMoreThisProduct(productInfo._id)}>
-                -
-              </button>
-              {selectedProducts.filter((id) => id === productInfo._id).length}
-              <button onClick={() => addMoreThisProduct(productInfo._id)}>
-                +
-              </button>
+        cartProducts.map((productInfo) => {
+          const amount = selectedProducts.filter((id) => id === productInfo._id).length
+          if(amount === 0) return
+          return (
+            <div key={productInfo._id}>
+              <h2>{productInfo.name}</h2>{' '}
+              <div>
+                <button onClick={() => removeMoreThisProduct(productInfo._id)}>
+                  -
+                </button>
+                {selectedProducts.filter((id) => id === productInfo._id).length}
+                <button onClick={() => addMoreThisProduct(productInfo._id)}>
+                  +
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-      <form></form>
-      <p>subtotal: {subtotal}</p>
-      <p>delivery: {delivery}</p>
-      <p>Total: {total}</p>
-      <button>pay: {total}</button>
+          )
+        })
+      }
+      <form action='/api/checkout' method='POST'>
+        <input name='adress' value={adress} onChange={(e) => setAdress(e.target.value)} type="text" placeholder='adress'/>
+        <input name='city' value={city} onChange={(e) => setCity(e.target.value)} type="text" placeholder='city'/>
+        <input name='name' value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='name'/>
+        <input name='email' value={email} onChange={(e) => setemail(e.target.value)} type="text" placeholder='email'/>
+        <p>subtotal: {subtotal}</p>
+        <p>delivery: {delivery}</p>
+        <p>Total: {total}</p>
+        <input type='hidden' name='products' value={selectedProducts.join(',')}/>
+        <button type='submit'>pay: {total}</button>
+      </form>
     </Layout>
   )
 }
